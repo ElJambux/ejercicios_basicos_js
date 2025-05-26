acc45d1f271ab007c4ca3d0c755cf073cec28be6
ElJambux <jairosoler15@gmail.com>
2025-05-26 08:02:01 +0200

    ejercicio 10 completado

---
+++ ejercicio10.js
@@ -0,0 +1,17 @@
+/**Calcular un promedio es una tarea extremadamente común, así que prueba a implementar esa funcionalidad en la siguiente función.
+
+Puedes usar este array para probarla: */
+
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(numberList) {
+  // Completar
+  let suma = 0;
+
+  for (let i = 0; i < numberList.length; i++) {
+    suma = suma + numberList[i];
+  }
+
+  return suma / numberList.length;
+}
+
+console.log("La media es de: " + average(numbers));
