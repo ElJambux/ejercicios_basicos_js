6c8886581952196574303fd45dd7d597dc43a847
ElJambux <jairosoler15@gmail.com>
2025-05-26 02:04:05 +0200

    ejercicio 1 completado

---
+++ ejercicio1.js
@@ -0,0 +1,41 @@
+/* 1.1 Define tu superhéroe favorito:
+/Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk". */
+
+let miSuperheroeFavorito;
+
+miSuperheroeFavorito = "Hulk";
+
+/* 1.2  Define un número:
+Crea una variable llamada numeroFavorito y asigna el valor 50. */
+
+let numeroFavorito;
+
+numeroFavorito = 50;
+
+/* 1.3 Define las dimensiones:
+Crea una variable llamada altura y asigna el valor 5.
+Crea una variable llamada longitud y asigna el valor 10. */
+
+let altura;
+
+altura = 5;
+
+let longitud;
+
+longitud = 10;
+
+/* Calcula la suma:
+Crea una variable llamada suma y asigna el valor de altura + longitud. */
+
+let suma;
+
+suma = altura + longitud;
+
+/* Imprime los resultados:
+Muestra todas las variables por consola. */
+
+console.log(miSuperheroeFavorito);
+console.log(numeroFavorito);
+console.log(altura);
+console.log(longitud);
+console.log(suma);
