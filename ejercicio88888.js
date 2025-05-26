a8c5485424e0831c54777abf0a72c6c0e0915a4f
ElJambux <jairosoler15@gmail.com>
2025-05-26 07:40:18 +0200

    ejercicio 9 completado

---
+++ ejercicio88888.js
@@ -0,0 +1,25 @@
+/**Buscar la palabra más larga: Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+Puedes usar este array para probar tu función: */
+
+const avengers = [
+  "Hulk",
+  "Thor",
+  "Iron Man",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(stringList) {
+  // Completar código
+  let palabraMasLarga = "pal1";
+
+  for (let i = 0; i < stringList.lenght; i++) {
+    if (stringList[i].length > palabraMasLarga.length) {
+      palabraMasLarga == stringList[i];
+    }
+  }
+  return console.log("Es: " + palabraMasLarga);
+}
+
+findLongestWord(avengers);
