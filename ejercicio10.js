fd1e54b7235d0a5a7633dbbc719794f92f647594
ElJambux <jairosoler15@gmail.com>
2025-05-26 16:27:16 +0200

    ejercicio 11 completado

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,5 +1,4 @@
 /**Calcular un promedio es una tarea extremadamente común, así que prueba a implementar esa funcionalidad en la siguiente función.
-
 Puedes usar este array para probarla: */
 
 const numbers = [12, 21, 38, 5, 45, 37, 6];
